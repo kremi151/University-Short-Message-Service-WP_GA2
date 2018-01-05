0c79c17bc678f6592cba086b4236d2095165395b
Michel Kremer <kremi151dev@gmail.com>
2018-01-05 10:59:40 +0100

    Fixed panel not expanding bug in class view in a hacky way

--- src/main/java/lu/mkremer/webprogsga/beans/ClassController.java
+++ src/main/java/lu/mkremer/webprogsga/beans/ClassController.java
@@ -24,7 +24,7 @@ import lu.mkremer.webprogsga.util.MessageHelper;
 
 @ManagedBean(name="viewclass")
 @ViewScoped
-public class ClassController implements Serializable{//TODO: Fix accordion expanding bug
+public class ClassController implements Serializable{
 
 	/**
 	 * 
